509bd62fbed629c1ea00cb63553bba10a7b6c2c6
flov3rh <floperh@gmail.com>
2019-10-15 10:14:37 -0500

    fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -25,21 +25,17 @@ def bubble_sort(arr)
 end
 
 def bubble_sort_by(arr)
-  if block_given?
-    swap = -1
-    while swap != 0
-      swap = 0
-      (0..(arr.length - 2)).each do |i|
-        if yield(arr[i], arr[i + 1]).positive?
-          arr[i], arr[i + 1] = arr[i + 1], arr[i]
-          swap += 1
-        end
+  raise 'No block given' unless block_given?
+  swap = -1
+  while swap != 0
+    swap = 0
+    (0..(arr.length - 2)).each do |i|
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swap += 1
       end
     end
-  else
-    raise 'No block given'
   end
-
   puts '--------------'
   output = arr.join(', ')
   puts "[#{output}]"
